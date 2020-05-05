8cdf4c4a046b8c7b7756baeb06e573a123f015be
Saiem Irfan <64668469+CursedKeyboard@users.noreply.github.com>
2020-05-05 19:41:45 -0400

    Update lstrip.py
    Updated doc string quotes to double quotes to follow PEP257 guidelines.
    Fixed method so that it strips more than 1 when necessary.
    Added parameter which allows modification of strip amount.

--- ytm/utils/lstrip.py
+++ ytm/utils/lstrip.py
@@ -2,23 +2,39 @@
 Module containing the utility function: lstrip
 '''
 
-def lstrip(string: str, sub_string: str) -> str:
-    '''
-    Left strip sub_string from string.
+def lstrip(string: str, sub_string: str, strip_count: int = 1) -> str:
+    """
+    Left strip strip_count sub_string's from <string>.
 
-    Strips a phrase instead of a set of characters
+    Strips a phrase instead of a set of characters.
 
     Args:
-        string: Source string to strip from
-        sub_string: Sub string to strip from string
+        string: Source string to strip from.
+        sub_string: Sub string to strip from string.
+        strip_count: Maximum number of substrings to strip. Defaults to one.
 
     Returns:
         string left-stripped of sub_string
-
-    Example:
-        >>> lstrip('someData', 'some')
+    
+    Raises:
+        ValueError: If strip_count is less than one.
+        
+    Examples:
+        >>> lstrip('someData', 'some', 1)
         'Data'
-        >>>
-    '''
-
-    return string[len(sub_string) if string.startswith(sub_string) else 0:]
+        >>> lstrip('somesomesomesom', 'some', 2)
+        'somesom'
+    """
+    if strip_count <= 0:
+        raise ValueError
+        
+    strip_counter = 0
+    
+    while string.startswith(substring):
+        if strip_counter > strip_count:
+            string = string[len(subs_tring):]
+            strip_counter += 1
+        else:
+            break
+     
+    return string

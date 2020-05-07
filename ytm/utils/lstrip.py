e111cfac1a7c224a9356e3af538df7ea53fbe61c
Saiem Irfan <Saiem.irfan.2000@gmail.com>
2020-05-07 17:36:15 -0400

    Clarified behaviour of parameter under certain conditions in docstring

--- ytm/utils/lstrip.py
+++ ytm/utils/lstrip.py
@@ -4,7 +4,7 @@ Module containing the utility function: lstrip
 
 def lstrip(string: str, sub_string: str, count: int = None) -> str:
     """
-    Left strip <count> <sub_string>'s from <string>.
+    Left strip <count> <sub_string>'s from <string>. If <count> is not >= 1 or not an integer, it will be set to one.
 
     Strips a phrase instead of a set of characters.
 
@@ -12,7 +12,7 @@ def lstrip(string: str, sub_string: str, count: int = None) -> str:
         string: Source string to strip from
         sub_string: Sub string to strip from string
         count: Maximum number of sub strings to strip
-            Default: 1
+            Default: None
 
     Returns:
         <string> left-stripped of <sub_string> <count> times
